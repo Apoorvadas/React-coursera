b85107555dd67ec79795b03f04ae379285717357
Apoorva Das <apoorvadas4444@gmail.com>
2020-05-14 01:46:45 +0530

    Assignment 4

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -1,14 +1,23 @@
 import React from "react";
-import {Breadcrumb,BreadcrumbItem,Card,CardBody,
-  CardHeader,Media} from "reactstrap";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  Card,
+  CardBody,
+  CardHeader,
+  Media
+} from "reactstrap";
 import { Link } from "react-router-dom";
+import { baseUrl } from "../shared/baseUrl";
+import { Loading } from "./LoadingComponent";
+import { Fade, Stagger } from "react-animation-components";
 
 function About(props) {
   function RenderLeader({ leader }) {
     return (
       <Media className="mt-5">
         <Media left className="mr-5">
-          <Media object src={leader.image} alt={leader.name} />
+          <Media object src={baseUrl + leader.image} alt={leader.name} />
         </Media>
         <Media body>
           <Media heading>{leader.name}</Media>
@@ -17,6 +26,23 @@ function About(props) {
         </Media>
       </Media>
     );
+  }
+
+  function RenderContent({ leaders, isLoading, errMess }) {
+    if (isLoading) {
+      return <Loading />;
+    } else if (errMess) {
+      return <h4>{errMess}</h4>;
+    } else
+      return (
+        <Stagger in>
+          {props.leaders.map(leader => (
+            <Fade in key={leader.id}>
+              <RenderLeader key={leader.id} leader={leader} />
+            </Fade>
+          ))}
+        </Stagger>
+      );
   }
 
   return (
@@ -96,13 +122,16 @@ function About(props) {
         </div>
         <div className="col-12">
           <Media list>
-            {props.leaders.map(leader => (
-              <RenderLeader key={leader.id} leader={leader} />
-            ))}
+            <RenderContent
+              leaders={props.leader}
+              isLoading={props.leaderLoading}
+              errMess={props.leaderErrMess}
+            />
           </Media>
         </div>
       </div>
     </div>
   );
 }
+
 export default About;
