ec04e5a4d4273f27efa2502c807bf31e4c2cb790
Apoorva Das <apoorvadas4444@gmail.com>
2020-05-13 19:39:27 +0530

    Single application part 2

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import { Card, CardImg, CardImgOverlay, CardText, CardBody,
-    CardTitle } from 'reactstrap';
-
-
+import { Card, CardImg, CardText, CardBody,
+    CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
     function RenderComments({comments}){
         if(comments == null){
@@ -61,14 +60,29 @@ import { Card, CardImg, CardImgOverlay, CardText, CardBody,
             return (<div></div>)
         }
 
-        return(
-            <div class="container">
-            <div className='row'>
-                <RenderDish dish={props.dish}/>
-                <RenderComments comments={props.dish.comments} />
+        return (
+            <div className="container">
+            <div className="row">
+                <Breadcrumb>
+
+                    <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                    <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                </Breadcrumb>
+                <div className="col-12">
+                    <h3>{props.dish.name}</h3>
+                    <hr />
+                </div>                
+            </div>
+            <div className="row">
+                <div className="col-12 col-md-5 m-1">
+                    <RenderDish dish={props.dish} />
+                </div>
+                <div className="col-12 col-md-5 m-1">
+                    <RenderComments comments={props.comments} />
+                </div>
             </div>
             </div>
-        )
+        );
     }
 
 
